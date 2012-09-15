754c1ad6161924770f14fa6f3d34b80b62221def
Ryo NAKAMURA <ryo-nakamura@cookpad.com>
2012-09-15 16:24:39 +0900

    Implement basic callback for on-message

--- lib/sara.rb
+++ lib/sara.rb
@@ -1,4 +1,23 @@
 require "sara/version"
 
 module Sara
+  module ClassMethods
+    def on_message(&block)
+      on_messages << block
+    end
+
+    def on_messages
+      @on_messages ||= []
+    end
+
+    def do_message(message)
+      on_messages.each do |callback|
+        callback.call(message)
+      end
+    end
+  end
+
+  def self.included(mod)
+    mod.extend(ClassMethods)
+  end
 end
--- spec/sara_spec.rb
+++ spec/sara_spec.rb
@@ -1,7 +1,54 @@
 require "spec_helper"
 
 describe Sara do
-  it do
-    true.should be_true
+  let(:klass) do
+    Class.new { include Sara }
+  end
+
+  describe ".included" do
+    it "should extend includer with Sara::ClassMethods" do
+      klass.singleton_class.ancestors.should include(described_class::ClassMethods)
+    end
+  end
+
+  describe ".on_message" do
+    it "should store given block as callback" do
+      expect do
+        klass.on_message { }
+      end.to change { klass.on_messages.size }.by(1)
+    end
+  end
+
+  describe ".on_messages" do
+    context "when no callback is stored" do
+      it do
+        klass.on_messages.should have(0).callback
+      end
+    end
+
+    context "when a callback is stored" do
+      before do
+        klass.on_message { }
+      end
+
+      it do
+        klass.on_messages.should have(1).callback
+      end
+    end
+  end
+
+  describe ".do_message" do
+    it "should trigger all callbacks registered as on_message" do
+      callback = proc {}
+      callback.should_receive(:call)
+      klass.stub(:on_messages) { [callback] }
+      klass.do_message(mock)
+    end
+
+    it "should trigger callbacks with given message" do
+      message = mock
+      klass.on_message { |callback| callback.should == message }
+      klass.do_message(message)
+    end
   end
 end
