fef535dfca40662cd7fcf0393cf6727bb5baa031
Ryo NAKAMURA <ryo-nakamura@cookpad.com>
2012-09-16 14:28:26 +0900

    Refactor test to be more simple

--- spec/chatroid_spec.rb
+++ spec/chatroid_spec.rb
@@ -4,20 +4,20 @@ describe Chatroid do
   describe "#set" do
     context "when given a hash" do
       it "should set it to config" do
-        described_class.new { set :key => "val" }.config.should == { :key => "val" }
+        Chatroid.new { set :key => "val" }.config.should == { :key => "val" }
       end
     end
 
     context "when given two args" do
       it "should set it to config as key-value config" do
-        described_class.new { set :key, "val" }.config.should == { :key => "val" }
+        Chatroid.new { set :key, "val" }.config.should == { :key => "val" }
       end
     end
 
     context "when given args more than two args" do
       it do
         expect do
-          described_class.new { set 1, 2, 3 }
+          Chatroid.new { set 1, 2, 3 }
         end.to raise_error(ArgumentError)
       end
     end
@@ -27,16 +27,16 @@ describe Chatroid do
     context "when service to connect is not specified" do
       it do
         expect do
-          described_class.new.run!
-        end.to raise_error(described_class::ConnectionError)
+          Chatroid.new.run!
+        end.to raise_error(Chatroid::ConnectionError)
       end
     end
 
     context "when specified service is not supported" do
       it do
         expect do
-          described_class.new { set :service, "NotSupportedService" }.run!
-        end.to raise_error(described_class::ConnectionError)
+          Chatroid.new { set :service, "NotSupportedService" }.run!
+        end.to raise_error(Chatroid::ConnectionError)
       end
     end
 
@@ -44,7 +44,7 @@ describe Chatroid do
       it "should find adapter and call #run! of it" do
         adapter = mock
         adapter.should_receive(:run!)
-        instance = described_class.new
+        instance = Chatroid.new
         instance.stub(:validate_connection)
         instance.stub(:adapter).and_return(adapter)
         instance.run!
