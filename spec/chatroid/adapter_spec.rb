0bdadc8e75b84bb7b8fcc13844860dca1f019d09
Ryo NAKAMURA <ryo-nakamura@cookpad.com>
2012-09-16 21:08:50 +0900

    Add spec for Chatroid::Adapter class method

--- spec/chatroid/adapter_spec.rb
+++ spec/chatroid/adapter_spec.rb
@@ -1,4 +1,25 @@
 require "spec_helper"
 
 describe Chatroid::Adapter do
+  describe ".find" do
+    context "when proper adapter class exists" do
+      before do
+        Chatroid::Adapter.stub(:const_get).and_return(adapter_class)
+      end
+
+      let(:adapter_class) do
+        Class.new
+      end
+
+      it "should return it" do
+        Chatroid::Adapter.find("AdapterClassName").should == adapter_class
+      end
+    end
+
+    context "when proper adapter class does not exist" do
+      it do
+        Chatroid::Adapter.find("AdapterClassName").should == nil
+      end
+    end
+  end
 end
