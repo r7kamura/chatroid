f9c14d4dd8733b1d72cce90b3b12245d77eb4c9a
r7kamura <r7kamura@gmail.com>
2012-11-20 08:13:15 +0900

    Fix wrong method name by mistake

--- examples/twitter_timer.rb
+++ examples/twitter_timer.rb
@@ -7,15 +7,15 @@ Chatroid.new do
   set :access_key,      "..."
   set :access_secret,   "..."
 
-  on_timer :hour => 6, :min => 0, :sec => 0 do
+  on_time :hour => 6, :min => 0, :sec => 0 do
     tweet "Good morning!"
   end
 
-  on_timer :hour => 0, :min => 0, :sec => 0 do
+  on_time :hour => 0, :min => 0, :sec => 0 do
     tweet "Good night!"
   end
 
-  on_timer :day => [10, 20, 30], :hour => 12, :min => 0, :sec => 0 do
+  on_time :day => [10, 20, 30], :hour => 12, :min => 0, :sec => 0 do
     tweet "@chatroid How are you?"
   end
 end.run!
